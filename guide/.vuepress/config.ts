7901c05cdc3615a3a96778b7348715e4b43d2374
Jaw0r3k <jaworekwiadomosci@gmail.com>
2022-10-08 18:59:56 +0200

    Basic pwa

--- guide/.vuepress/config.ts
+++ guide/.vuepress/config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { defineUserConfig } from 'vuepress-vite';
 import type { DefaultThemeOptions, ViteBundlerOptions } from 'vuepress-vite';
 import sidebar from './sidebar';
-
+import { pwaPlugin } from '@vuepress/plugin-pwa'
 const config = defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
 	bundler: '@vuepress/vite',
 	templateDev: path.join(__dirname, 'templates', 'index.dev.html'),
@@ -11,6 +11,10 @@ const config = defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
 	title: 'discord.js Guide',
 	description: 'Imagine a guide... that explores the many possibilities for your discord.js bot.',
 	head: [
+		['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+		['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+		['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+		['link', { rel: 'apple-touch-icon', href: '/meta-image.png' }],	
 		['meta', { charset: 'utf-8' }],
 		['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
 		['link', { rel: 'icon', href: '/favicon.png' }],
@@ -46,11 +50,16 @@ const config = defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
 			mediumZoom: false,
 		},
 	},
-	plugins: [],
+	plugins: [[
+		  '@vuepress/pwa',
+		  {
+			mode: 'CacheFirst'
+		  }
+	]
+	],
 });
 
 const { ALGOLIA_DOCSEARCH_API_KEY, ALGOLIA_DOCSEARCH_APP_ID, GOOGLE_ANALYTICS_ID, NODE_ENV } = process.env;
-
 if (NODE_ENV === 'production' && ALGOLIA_DOCSEARCH_API_KEY && GOOGLE_ANALYTICS_ID) {
 	config.plugins.push(
 		[
